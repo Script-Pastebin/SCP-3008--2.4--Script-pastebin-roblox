5cbaf92dccd58448dee9c2dadc936c967fe13283
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-25 07:31:57 +0100

    commit zero

---
+++ SCP-3008 [2.4] Script pastebin roblox SCP-3008 Gui .lua
@@ -0,0 +1,9 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Daniel-Herron/Wulphram/main/SCP3008.lua",true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+It has:1)No fall damage, 2)remove energy depletion, 3) inf. jump and more!
+]]
